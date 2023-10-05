35e2003130f4bdd6826a6b4b8f58d25b1d41ffc5
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-10-05 12:12:12 -0500

    cambios finales escalabilidad

--- interfaceapp/urls.py
+++ interfaceapp/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.vista_login, name='login'),
     path('pagina_principal/', views.vista_principal, name='principal')
 ]
--- interfaceapp/views.py
+++ interfaceapp/views.py
@@ -1,8 +1,6 @@
 import requests
 from django.shortcuts import render, redirect
 from django import forms
-from interfaceapp import producer
-from interfaceapp import subscriber
 
 def vista_login(request):
 
@@ -33,55 +31,19 @@ def vista_login(request):
                     elif respuesta == "invalido":
                         mensaje_error = "Usuario/Clave incorrecto"
                         contexto = {'form': form, 'error_message': mensaje_error}
-                        return render(request, 'pagina_login_nuevo.html', contexto)
+                        return render(request, 'pagina_login.html', contexto)
                     
                 else:
                     mensaje_error = "Error en la solicitud al servidor de autenticación"
                     contexto = {'form': form, 'error_message': mensaje_error}
-                    return render(request, 'pagina_login_nuevo.html', contexto)
+                    return render(request, 'pagina_login.html', contexto)
                 
             except requests.exceptions.RequestException as e:
                 mensaje_error = "Error de conexión con el servidor de autenticación"
                 contexto = {'form': form, 'error_message': mensaje_error}
-                return render(request, 'pagina_login_nuevo.html', contexto)
+                return render(request, 'pagina_login.html', contexto)
 
-    return render(request, 'pagina_login_nuevo.html')
-
-"""
-def vista_login(request):
-
-    if request.method == 'POST':
-
-        form = LoginForm(request.POST)
-
-        if form.is_valid():
-
-            usuario = form.cleaned_data['usuario']
-            clave = form.cleaned_data['clave']
-
-            print("> usuario: " + usuario + ", clave: " + clave)
-
-            producer.enviar_peticion_autenticacion(usuario, clave)
-            subscriber.respuesta_autenticacion = ""  # Restablece la respuesta
-            subscriber.detener_consumo = False  # Restablece la bandera
-            subscriber.recibir_respuesta_autenticacion() 
-            response = subscriber.respuesta_autenticacion
-            subscriber.detener_consumo = True
-
-            print("> La respuesta de la solicitud es: " + response)
-
-            if response == "VALIDO":
-                return redirect('principal')
-            elif response == "INVALIDO":
-                error_message = "Usuario/Clave incorrecto"
-                context = {'form': form, 'error_message': error_message}
-                return render(request, 'pagina_login_nuevo.html', context)
-                
-    else:
-        form = LoginForm()
-
-    return render(request, 'pagina_login_nuevo.html')
-"""
+    return render(request, 'pagina_login.html')
 
 def vista_principal(request):
     return render(request, 'pagina_principal.html')
