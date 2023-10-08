a1c7322f77cc77dd8a30d3e3d54e1e0b415e3ab4
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-10-08 17:22:56 -0500

    cambios html

--- interfaceapp/urls.py
+++ interfaceapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.vista_login, name='login'),
+    path('', views.vista_login),
     path('principal_profesionalSalud/', views.vista_principal_profesionalSalud, name='principal_profesionalSalud'),
     path('principal_paciente/', views.vista_principal_paciente, name='principal_paciente'),
     path('principal_director/', views.vista_principal_director, name='principal_director')
