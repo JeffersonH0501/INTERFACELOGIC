bbac83123268223a4b8e4e6d4d198c801107a888
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-10-20 13:32:57 -0500

    cambios

--- interfaceapp/views.py
+++ interfaceapp/views.py
@@ -3,6 +3,61 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django import forms
 
+
+
+def agregar_adenda(request):
+
+    contexto = {}
+   
+    contexto['foto'] = request.session.get('foto')
+    contexto['nombre'] = request.session.get('nombre')
+    documento_profesional = request.session.get('documento')
+    contexto['documento'] = documento_profesional
+    contexto['edad'] = request.session.get('edad')
+    contexto['telefono'] = request.session.get('telefono')
+    contexto['sexo'] = request.session.get('sexo')
+
+    form = AdendaForm(request.POST)
+
+    if form.is_valid():
+
+        documento_paciente = form.cleaned_data['documento_paciente']
+        fecha = form.cleaned_data['fecha']
+        tipo = form.cleaned_data['tipo']
+        descripcion = form.cleaned_data['descripcion']
+
+        informacion_adenda = {'documento_paciente': documento_paciente, 'documento_profesional': documento_profesional, 'fecha': fecha, 'tipo': tipo, 'descripcion': descripcion}
+
+        print("> documento: " + documento_paciente + ", documento_profesional: " + documento_profesional + ", fecha: " + fecha + ", tipo: " + tipo)
+
+        url_agregar_adenda = 'http://10.128.0.6:8080/agregarAdenda/' #URL del servidor de usuarios
+
+        try:
+
+            respuestaHttp = requests.post(url_agregar_adenda, json=informacion_adenda)
+
+            if respuestaHttp.status_code == 200:
+
+                adenda = respuestaHttp.json().get('adenda')
+
+                if adenda == None:
+                    print("Adenda No fue agrega con exito al paciente con documento:", documento_paciente)
+                    contexto['mensaje'] = "El paciente no existe/El paciente no le pertenece"
+                else:
+                    print("Adenda fue agregada con exito al paciente con documento:", documento_paciente)
+                    print("Información de la Adenda:", adenda)
+                    contexto['mensaje'] = "Adenda agregada con exito"
+
+            else:
+                contexto['mensaje'] = "Error en la solicitud al servidor de usuarios"
+                
+        except requests.exceptions.RequestException as e:
+            contexto['mensaje'] = "Error de conexión con el servidor de usuarios"
+
+    return render(request, 'pagina_agregar_adenda', contexto)
+    
+
+
 def vista_login(request):
 
     if request.method == 'POST':
@@ -64,7 +119,58 @@ def vista_login(request):
 
 def vista_agregar_adenda(request):
 
-    documento = request.session.get('documento')
+    if request.method == 'POST':
+        agregar_adenda(request)
+
+    else:
+
+        documento = request.session.get('documento')
+        url_usuario = 'http://10.128.0.6:8080/usuario/' #URL del servidor de usuarios
+
+        try:
+
+            respuestaHttp = requests.post(url_usuario, json={'documento': documento})
+
+            if respuestaHttp.status_code == 200:
+
+                usuarioJson = respuestaHttp.json()
+
+                usuario = {
+                    'documento': usuarioJson.get('documento'),
+                    'clave': usuarioJson.get('clave'),
+                    'tipo': usuarioJson.get('tipo'),
+                    'foto': usuarioJson.get('foto'),
+                    'nombre': usuarioJson.get('nombre'),
+                    'edad': usuarioJson.get('edad'),
+                    'telefono': usuarioJson.get('telefono'),
+                    'sexo': usuarioJson.get('sexo'),
+                }
+
+                if usuario['tipo'] == 'profesionalSalud':
+
+                    request.session['foto'] = usuario['foto']
+                    request.session['nombre'] = usuario['nombre']
+                    request.session['edad'] = usuario['edad']
+                    request.session['telefono'] = usuario['telefono']
+                    request.session['sexo'] = usuario['sexo']
+
+                    return render(request, 'pagina_agregar_adenda.html', usuario)
+
+                else:
+                    request.session['mensaje_error'] = f"Error al cargar la pagina ya que el {documento}  no corresponde a un profesional de salud"
+
+            else:
+                request.session['mensaje_error'] = f"Error ({respuestaHttp.status_code}) al cargar la página del profesional de salud"
+
+        except requests.exceptions.RequestException as e:
+            request.session['mensaje_error'] = "Error al cargar la pagina del profesional de salud"
+        
+        return redirect(reverse('pagina_error'))
+    
+
+
+def vista_principal_paciente(request, documento):
+
     url_usuario = 'http://10.128.0.6:8080/usuario/' #URL del servidor de usuarios
 
     try:
@@ -86,101 +192,64 @@ def vista_agregar_adenda(request):
                 'sexo': usuarioJson.get('sexo'),
             }
 
-            if usuario['tipo'] == 'profesionalSalud':
-
-                request.session['foto'] = usuario['foto']
-                request.session['nombre'] = usuario['nombre']
-                request.session['edad'] = usuario['edad']
-                request.session['telefono'] = usuario['telefono']
-                request.session['sexo'] = usuario['sexo']
-
-                return render(request, 'pagina_agregar_adenda,html', usuario)
+            if usuario['tipo'] == 'paciente':
+                return render(request, 'pagina_principal_paciente.html', usuario)
 
             else:
-                request.session['mensaje_error'] = f"Error al cargar la pagina ya que el {documento}  no corresponde a un profesional de salud"
+                request.session['mensaje_error'] = f"Error al cargar la pagina ya que el {documento}  no corresponde a un paciente"
 
         else:
-            request.session['mensaje_error'] = f"Error ({respuestaHttp.status_code}) al cargar la página del profesional de salud"
+            request.session['mensaje_error'] = f"Error ({respuestaHttp.status_code}) al cargar la página del paciente"
 
     except requests.exceptions.RequestException as e:
-        request.session['mensaje_error'] = "Error al cargar la pagina del profesional de salud"
-    
+        request.session['mensaje_error'] = "Error al cargar la pagina del paciente"
+        
     return redirect(reverse('pagina_error'))
-    
-
-
-def vista_principal_paciente(request, documento):
-    return render(request, 'pagina_principal_paciente.html', {'documento': documento})
 
 
 
 def vista_principal_director(request, documento):
-    return render(request, 'pagina_principal_director.html', {'documento': documento})
+
+    url_usuario = 'http://10.128.0.6:8080/usuario/' #URL del servidor de usuarios
+
+    try:
+
+        respuestaHttp = requests.post(url_usuario, json={'documento': documento})
+
+        if respuestaHttp.status_code == 200:
+
+            usuarioJson = respuestaHttp.json()
+
+            usuario = {
+                'documento': usuarioJson.get('documento'),
+                'clave': usuarioJson.get('clave'),
+                'tipo': usuarioJson.get('tipo'),
+                'foto': usuarioJson.get('foto'),
+                'nombre': usuarioJson.get('nombre'),
+                'edad': usuarioJson.get('edad'),
+                'telefono': usuarioJson.get('telefono'),
+                'sexo': usuarioJson.get('sexo'),
+            }
+
+            if usuario['tipo'] == 'paciente':
+                return render(request, 'pagina_principal_director.html', usuario)
+
+            else:
+                request.session['mensaje_error'] = f"Error al cargar la pagina ya que el {documento}  no corresponde a un director"
+
+        else:
+            request.session['mensaje_error'] = f"Error ({respuestaHttp.status_code}) al cargar la página del director"
+
+    except requests.exceptions.RequestException as e:
+        request.session['mensaje_error'] = "Error al cargar la pagina del director"
+        
+    return redirect(reverse('pagina_error'))
 
 
 
 def vista_error(request):
     mensaje_error = request.session.get('mensaje_error')
     return render(request, 'pagina_error.html', {'error_message': mensaje_error})
-
-
-
-def agregar_adenda(request):
-
-    contexto = {}
-   
-    if request.method == 'POST':
-
-        contexto['foto'] = request.session.get('foto')
-        contexto['nombre'] = request.session.get('nombre')
-        documento_profesional = request.session.get('documento')
-        contexto['documento'] = documento_profesional
-        contexto['edad'] = request.session.get('edad')
-        contexto['telefono'] = request.session.get('telefono')
-        contexto['sexo'] = request.session.get('sexo')
-
-        form = AdendaForm(request.POST)
-
-        if form.is_valid():
-
-            documento_paciente = form.cleaned_data['documento_paciente']
-            fecha = form.cleaned_data['fecha']
-            tipo = form.cleaned_data['tipo']
-            descripcion = form.cleaned_data['descripcion']
-
-            informacion_adenda = {'documento_paciente': documento_paciente, 'documento_profesional': documento_profesional, 'fecha': fecha, 'tipo': tipo, 'descripcion': descripcion}
-
-            print("> documento: " + documento_paciente + ", documento_profesional: " + documento_profesional + ", fecha: " + fecha + ", tipo: " + tipo)
-
-            url_agregar_adenda = 'http://10.128.0.6:8080/agregarAdenda/' #URL del servidor de usuarios
-
-            try:
-
-                respuestaHttp = requests.post(url_agregar_adenda, json=informacion_adenda)
-
-                if respuestaHttp.status_code == 200:
-
-                    adenda = respuestaHttp.json().get('adenda')
-
-                    if adenda == None:
-                        print("Adenda No fue agrega con exito al paciente con documento:", documento_paciente)
-                        contexto['mensaje'] = "El paciente no existe/El paciente no le pertenece"
-                    else:
-                        print("Adenda fue agregada con exito al paciente con documento:", documento_paciente)
-                        print("Información de la Adenda:", adenda)
-                        contexto['mensaje'] = "Adenda agregada con exito"
-
-                else:
-                    contexto['mensaje'] = "Error en la solicitud al servidor de usuarios"
-                
-            except requests.exceptions.RequestException as e:
-                contexto['mensaje'] = "Error de conexión con el servidor de usuarios"
-
-        return render(request, 'pagina_principal_profesionalSalud.html', contexto)
-    
-    else:
-        request.session['mensaje_error'] = "Error al agregar la agenda. La peticion no es POST" 
-        return redirect(reverse('pagina_error'))
 
 
 
