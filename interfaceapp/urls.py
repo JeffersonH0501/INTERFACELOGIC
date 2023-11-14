ca467413a3c0f1ae329418c14ac08d3b6cf86198
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-14 18:59:08 -0500

    cambios

--- interfaceapp/urls.py
+++ interfaceapp/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path("principal_profesionalSalud/<str:documento>/", logica_profesionalSalud.vista_principal_profesionalSalud, name="principal_profesionalSalud"),
     path('principal_director/<str:documento>/', logica_director.vista_principal_director, name="principal_director"),
 
+    path("principal_profesionalSalud/<str:documento>/historia_clinica/<str:documento_paciente>", logica_profesionalSalud.vista_principal_profesionalSalud, name="principal_profesionalSalud"),
+
+
     path("historia_clinica/<str:documento>/", logica_paciente.vista_historia_clinica, name = "historia_clinica"),
     path("agregar_adenda/<str:documento>/", logica_profesionalSalud.vista_agregar_adenda, name="agregar_adenda"),
 
