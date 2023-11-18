62983a5e84df9eca149301b823ea28df7aa7ef7e
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-18 08:36:16 -0500

    cambios

--- interfaceapp/settings.py
+++ interfaceapp/settings.py
@@ -83,9 +83,10 @@ STATICFILES_DIRS = (
 )
 
 #sprint3
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Servidor SMTP de Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jefferson05012004@gmail.com'
-EMAIL_HOST_PASSWORD = 'Jefferson.12'
+EMAIL_HOST_PASSWORD = 'fxkt xtym nqsb osja'
+'
