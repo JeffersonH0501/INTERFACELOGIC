8d0ac0cc35e65625384faf156459719d95657b75
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-18 08:17:32 -0500

    cambos

--- interfaceapp/settings.py
+++ interfaceapp/settings.py
@@ -87,5 +87,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Servidor SMTP de Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'jefferson05012004@gmail.com'
 EMAIL_HOST_PASSWORD = 'Jefferson.12'
