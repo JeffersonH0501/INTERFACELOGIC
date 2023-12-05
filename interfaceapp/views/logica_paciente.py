7e81c98a17971431986f3fed65b863a0a2f01ea0
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-12-04 22:06:10 -0500

    cambios

--- interfaceapp/views/logica_paciente.py
+++ interfaceapp/views/logica_paciente.py
@@ -46,6 +46,7 @@ def consultar_historia(request, documento):
 
             respuestaJson = respuestaHttp.json()
             historia = respuestaJson.get('historia_clinica')
+            print(historia)
             if historia is not None:
                 request.session["usuario"]["historia_clinica"] = historia
             else:
